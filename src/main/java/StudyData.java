093a73043c44806442def7fd3dc49ff4b08b344c
flystar <1787511059@qq.com>
2022-06-20 16:04:51 +0800

    context

---
+++ src/main/java/StudyData.java
@@ -0,0 +1,15 @@
+/**
+ * @author flystar
+ * @date 2022/6/20  16:00
+ * @project TestTwo
+ * @company
+ */
+public class StudyData {
+    public static void main(String[] args) {
+        System.out.println("0");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+    }
+}
